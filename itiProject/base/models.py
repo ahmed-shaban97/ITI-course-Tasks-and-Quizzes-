847900de388e046a832d2051d80e1dc8bf966010
Ahmed Shaban <ahmedjd2025@gmail.com>
2025-04-10 14:53:46 +0200

    Upload the final project

---
+++ itiProject/base/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title =
+    descriptions =
+# Create your models here.
